02597a3d3d46cc260750d39cc63048fc2e421f3b
malanek <malanek333@gmail.com>
2024-11-05 12:34:02 +0100

    Optimizations

--- Drawer/DOTweenParamsDrawer.cs
+++ Drawer/DOTweenParamsDrawer.cs
@@ -7,61 +7,35 @@ namespace BBExtensions.DOTweenExt
     [CustomPropertyDrawer(typeof(DOTweenParams))]
     public class DOTweenParamsDrawer : PropertyDrawer
     {
+        private const string customEase = nameof(DOTweenParams.CustomEase);
+        private const string animationCurve = nameof(DOTweenParams.AnimationCurve);
+        private const string standardEase = nameof(DOTweenParams.StandardEase);
+        private const string duration = nameof(DOTweenParams.Duration);
+        private const float optionWidth = 15f;
+        private const float durationWidth = 50f;
+        private const float labelWidth = 125f;
+        private const float padding = 2f;
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            EditorGUI.BeginProperty(position, label, property);
+            float remainingWidth = position.width - labelWidth - optionWidth - durationWidth - 6f;
+            Rect labelRect = new(position.x, position.y, labelWidth, position.height);
+            Rect optionRect = new(position.x + labelWidth, position.y, optionWidth, position.height);
+            Rect curveRect = new(position.x + labelWidth + optionWidth + padding, position.y, remainingWidth, position.height);
+            Rect durationRect = new(position.x + labelWidth + optionWidth + padding + remainingWidth + padding, position.y, durationWidth, position.height);
 
-            // Get the element index if it's part of a list
-            int index = GetElementIndex(property);
-
-            float labelWidth = EditorGUIUtility.labelWidth;
-            float indentLength = EditorGUI.indentLevel * 15f;
-
-            // Display the variable name or list element index
-            Rect labelRect = new(position.x + indentLength, position.y, labelWidth - indentLength, position.height);
-            if (index != -1)
+            using (new EditorGUI.PropertyScope(position, label, property))
             {
-                EditorGUI.LabelField(labelRect, new GUIContent("Element " + index));
-            }
-            else
-            {
+                SerializedProperty customEaseProperty = property.FindPropertyRelative(customEase);
+                SerializedProperty durationProperty = property.FindPropertyRelative(duration);
                 EditorGUI.LabelField(labelRect, label);
+                EditorGUI.PropertyField(optionRect, customEaseProperty, GUIContent.none);
+                EditorGUI.PropertyField(durationRect, durationProperty, GUIContent.none);
+                if (customEaseProperty.boolValue)
+                    EditorGUI.PropertyField(curveRect, property.FindPropertyRelative(animationCurve), GUIContent.none);
+                else
+                    EditorGUI.PropertyField(curveRect, property.FindPropertyRelative(standardEase), GUIContent.none);
             }
-
-            float optionWidth = 15f; // Fixed width for option field
-            float durationWidth = 50f; // Fixed width for duration field
-
-            float remainingWidth = position.width - labelWidth - optionWidth - durationWidth - 4f; // Remaining width for curve field
-            float startX = position.x + labelWidth;
-
-            Rect optionRect = new(startX, position.y, optionWidth, position.height);
-            Rect curveRect = new(startX + optionWidth + 2f, position.y, remainingWidth, position.height);
-            Rect durationRect = new(startX + optionWidth + 2f + remainingWidth + 2f, position.y, durationWidth, position.height);
-
-            SerializedProperty customEaseProperty = property.FindPropertyRelative("CustomEase");
-
-            EditorGUI.PropertyField(optionRect, customEaseProperty, GUIContent.none);
-            if (customEaseProperty.boolValue)
-                EditorGUI.PropertyField(curveRect, property.FindPropertyRelative("AnimationCurve"), GUIContent.none);
-            else
-                EditorGUI.PropertyField(curveRect, property.FindPropertyRelative("StandardEase"), GUIContent.none);
-            EditorGUI.PropertyField(durationRect, property.FindPropertyRelative("Duration"), GUIContent.none);
-
-            EditorGUI.EndProperty();
-        }
-
-        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => EditorGUIUtility.singleLineHeight;
-
-        private int GetElementIndex(SerializedProperty property)
-        {
-            if (property.depth == 0 || !property.propertyPath.Contains("["))
-                return -1;
-
-            int startIndex = property.propertyPath.LastIndexOf("[") + 1;
-            int endIndex = property.propertyPath.IndexOf("]", startIndex);
-            string indexString = property.propertyPath[startIndex..endIndex];
-
-            return int.TryParse(indexString, out int index) ? index : -1;
         }
 
     }
